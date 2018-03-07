62c0ec75dcc0392627251a82152deb32335c7213
Gabriel Ronaldo Viza <vmgronaldo@gmail.com>
2018-03-07 11:04:57 -0500

    Validacion de Archivo
    En esta parte se valida los archivos que se enviar en cotizacion y llegaran al correo de sugeinsa ventas
    se valido las extensiones y un peso de archivo limitado .

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -8,17 +8,65 @@ $(document).ready(function () {
   $("#frmCotizacion").on("submit", function (e) {
     cotizacion(e);
 
+
   });
 
 
+  $("#adjunto").change(function () {
+
+    var archivo = $("#adjunto").val();
+    var extensiones = archivo.substring(archivo.lastIndexOf("."));
+    if (extensiones != ".docx" && extensiones != ".txt" && extensiones != ".pdf" && extensiones != ".xlsx" && extensiones != ".png" && extensiones != ".jpg" && extensiones != ".doc") {
+      swal({
+      position: 'center',
+      type: 'error',
+      title: 'Validacion de Archivo Cargado',
+      text: "¡Archivos Permitidos doc , docx , xlsx , pdf , csv , txt , png ,  jpg , jpeg Gracias! ",
+      showConfirmButton: false,
+      animation: true,
+      customClass: 'bounceOut',
+      timer: 4500
+    })
+    limpiar() ;
+    }else{
+      var file_size = $('#adjunto')[0].files[0].size;
+      if (file_size > 2097152) {
+        swal({
+          position: 'center',
+          type: 'error',
+          title: 'mensaje de archivo cargado',
+          text: "¡Pórfavor el peso maximo de archivo es de 2 MB gracias! ",
+          showConfirmButton: false,
+          animation: true,
+          customClass: 'bounceOut',
+          timer: 3500
+        })
+  
+      } else {
+        swal({
+          position: 'center',
+          type: 'success',
+          title: 'mensaje de archivo cargado',
+          text: "¡Su archivo cumple los requisitos , puede dar click en enviar Gracias! ",
+          showConfirmButton: false,
+          animation: true,
+          customClass: 'bounceOut',
+          timer: 3500
+        })
+      }
+    }
+  });
 })
 
 function limpiar() {
   $("#nombre").val("");
   $("#email").val("");
-  $("#email").val("");
+  $("#nombre2").val("");
+  $("#email2").val("");
+  $("#celular").val("");
   $("#asunto").val("");
   $("#mensaje").val("");
+  $("#adjunto").val("");
 
 }
 
